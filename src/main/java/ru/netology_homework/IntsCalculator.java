f8ef2d34c58b513bbfc8d67745159c084741dada
dmitriy.pirumov <dmitry.pirumov@gmail.com>
2023-10-01 01:09:56 +0300

    init commit

---
+++ src/main/java/ru/netology_homework/IntsCalculator.java
@@ -0,0 +1,23 @@
+package ru.netology_homework;
+
+public class IntsCalculator implements Ints{
+    Calculator calculator;
+    public IntsCalculator (){
+        this.calculator = new Calculator();
+    }
+
+    @Override
+    public int sum(int arg0, int arg1) {
+        return (int) Math.round(calculator.newFormula().addOperand(arg0).addOperand(arg1).calculate(Calculator.Operation.SUM).result);
+    }
+
+    @Override
+    public int mult(int arg0, int arg1) {
+        return (int) Math.round(calculator.newFormula().addOperand(arg0).addOperand(arg1).calculate(Calculator.Operation.MULT).result);
+    }
+
+    @Override
+    public int pow(int a, int b) {
+        return (int) Math.round(calculator.newFormula().addOperand(a).addOperand(b).calculate(Calculator.Operation.POW).result);
+    }
+}
